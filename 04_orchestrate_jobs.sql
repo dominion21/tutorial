0c36a52404d44a48e9d06b04fc08ce33289c4b90
dominion21 <133910589+dominion21@users.noreply.github.com>
2024-08-30 12:34:58 -0400

    Create 04_orchestrate_jobs.sql

---
+++ 04_orchestrate_jobs.sql
@@ -0,0 +1,16 @@
+use role accountadmin;
+use schema RAW_prod.gold;
+
+
+-- declarative target table of pipeline
+create or alter table vacation_spots (
+    city varchar
+  , airport varchar
+  , co2_emissions_kg_per_person float
+  , punctual_pct float
+  , avg_temperature_air_f float
+  , avg_relative_humidity_pct float
+  , avg_cloud_cover_pct float
+  , precipitation_probability_pct float
+  -- STEP 5: INSERT CHANGES HERE
+) data_retention_time_in_days = 1;
