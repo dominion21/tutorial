e3aca848063b579ec5fdb34e6683d4d240586f47
mike <kambj88@yahoo.com>
2024-08-30 10:56:23 -0400

    insert customer data

---
+++ insert_customers.sql
@@ -0,0 +1,7 @@
+-- Create file "insert_customers.sql" and commit it to your git repo
+-- The file should have the following contents:
+INSERT INTO customer VALUES 
+  (1, 'Jane', 'Doe'),
+  (2, 'Max', 'Mustermann'),
+  (3, 'Jean', 'Dupont')
+;
