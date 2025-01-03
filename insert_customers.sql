1b213ae9a4574e474592a45f22a6e0cf95aaabb9
dominion21 <133910589+dominion21@users.noreply.github.com>
2025-01-03 14:01:27 -0500

    Delete insert_customers.sql

--- insert_customers.sql
+++
@@ -1,7 +0,0 @@
--- Create file "insert_customers.sql" and commit it to your git repo
--- The file should have the following contents:
-INSERT INTO customer VALUES 
-  (1, 'Jane', 'Doe'),
-  (2, 'Max', 'Mustermann'),
-  (3, 'Jean', 'Dupont')
-;
