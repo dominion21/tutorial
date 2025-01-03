06c9bb135a9627d3c2f70edfc37badb781c7afa4
dominion21 <133910589+dominion21@users.noreply.github.com>
2025-01-03 14:01:49 -0500

    Delete create_customer_table.sql

--- create_customer_table.sql
+++
@@ -1,8 +0,0 @@
--- Create file "create_customer_table.sql" and commit it to your git repo
--- The file should have the following contents:
-
-CREATE TABLE customer (
-  id number not null, 
-  first_name varchar, 
-  last_name varchar
-);
